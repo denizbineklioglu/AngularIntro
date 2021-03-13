2a9561c291ffc0e8dfcc6df7e482801c25ed98c5
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-13 22:41:10 +0300

    13.03 2

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,6 +10,8 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categories:Category[]=[];
+  currentCategory:Category;
+
   constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
@@ -22,5 +24,17 @@ export class CategoryComponent implements OnInit {
     .subscribe((response)=>{
     this.categories=response.data
     })
+  }
+
+  setCurrentCategory(category:Category){
+    this.currentCategory =category;
+  }
+
+  getCurrentCategoryClass(category:Category){
+     if(category == this.currentCategory){
+      return "list-group-item active" 
+     } else{
+       return "list-group-item"
+     }
   }
 }
