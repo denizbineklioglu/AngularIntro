b81ffec31e77acaf48aa2eae803f01669edfa542
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-17 21:48:08 +0300

    17.03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { CategoryComponent } from './components/category/category.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     NaviComponent,
-    CategoryComponent
+    CategoryComponent,
+    VatAddedPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -13,6 +13,8 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded=false;
+  filterText="";
+  
   constructor(private productService:ProductService,
               private activatedRoute:ActivatedRoute) { 
   }
