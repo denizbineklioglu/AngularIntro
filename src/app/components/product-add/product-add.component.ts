714e32cb7907f21dce7f9aa60c970b5d10232a87
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-20 23:04:27 +0300

    last

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder,FormControl,Validators} from "@angular/forms"
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 
 
@@ -11,7 +13,9 @@ import {FormGroup,FormBuilder,FormControl,Validators} from "@angular/forms"
 export class ProductAddComponent implements OnInit {
 
   productAddForm:FormGroup
-  constructor(private formsBuilder:FormBuilder) { }
+  constructor(private formsBuilder:FormBuilder,
+              private productService:ProductService,
+              private toatrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -26,4 +30,20 @@ export class ProductAddComponent implements OnInit {
      })
   }
 
+  add(){
+    if(this.productAddForm.valid){
+      let productModel =Object.assign({},this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response=>{
+        console.log(response);
+        this.toatrService.success(response.message,"Başarılı")
+      },responseError=>{
+        console.log(responseError)
+        this.toatrService.error(responseError.error)
+      })
+
+    } else {
+      this.toatrService.error("Formunuz Eksik","Dikkat")
+    }
+
+  }
 }
