0dc119ac7df67e7300ea06482d38e49b3a79caa1
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-24 23:04:08 +0300

    commit 4

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ListResponseModel } from '../models/ListResponseModel';
+import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
 import { ResponseModel } from '../models/responseModel';
 
